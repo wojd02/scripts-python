fa95512c033dde2b178abd02f20240dc46bfa280
wojd02 <jonatanbayere@gmail.com>
2025-03-20 11:57:37 -0300

    Tratando dados e fazendo contas

---
+++ Exercicios/Desafio-005-015t.py
@@ -0,0 +1,73 @@
+#Sucessor e antescessor
+#n = int (input('Digite um número: '))
+#suc = n + 1
+#ant = n - 1
+#print('O antecessor do {} é {} e o sucessor é {}'.format(n, ant, suc))
+
+#dobro e triplo
+#n2 = float(input('Digite um número: '))
+#dob = n2 * 2
+#tri = n2 * 3
+#rai = n2**(1/2)
+#print('O dobro de {} é {}, o triplo é {} e a raíz quadrada é {}'.format(n2, dob, tri, rai))
+
+#Nota e média aluno
+#name = (input('Digite o nome do aluno: '))
+#not1 = float(input('Digite a primera nota: '))
+#not2 = float(input('Digite a segunda nota: '))
+#med = (not1 + not2) / 2
+#print('A média do aluno(a) {} com as respectivas notas {} e {} foi de {}'.format(name, not1, not2, med))
+
+#Conversor m para cm e mm
+#met = float(input('Digite o valor em metros(m): '))
+#cem = met * 100
+#milm = met * 1000
+#print('{}m equivalem a {}cm e {}mm'.format(met, cem, milm))
+
+#Tábuada sem repetição
+#n3 = int(input('Digite um número: '))
+#print(n3, ' X 1 =', (n3*1))
+#print(n3, ' X 2 =', (n3*2))
+#print(n3, ' X 3 =', (n3*3))
+#print(n3, ' X 4 =', (n3*4))
+#print(n3, ' X 5 =', (n3*5))
+#print(n3, ' X 6 =', (n3*6))
+#print(n3, ' X 7 =', (n3*7))
+#print(n3, ' X 8 =', (n3*8))
+#print(n3, ' X 9 =', (n3*9))
+#print(n3, ' X 10 =', (n3*10))
+
+#conversor real pra dólar
+#rea = float(input('Quantos reais você tem?: '))
+#dol = rea / 3.27
+#print('Com R${} você consegue comprar US${:.2f}'.format(rea, dol))
+
+#litragem tinta
+#larg = float(input('Qual a largura da parede? (em metros): '))
+#altu = float(input('Qual a altura da parede? (em metros): '))
+#are = larg * altu
+#tint = are / 2
+#print('A parede a ser pintada tem uma área de {:.2f}m² e vai precisar de {:.2f}L de tinta para cobrir toda a parece'. format(are, tint))
+
+#Desconto 5%
+#prec = float(input('Qual o preço do produto?: '))
+#desc = (prec * 5) / 100
+#nprec = prec - desc
+#print('O produto está por R${}'.format(nprec))
+
+#Reajuste salário 15%
+#sal = float(input('Qual o salário do funcionário: '))
+#acr = (sal * 15) / 100
+#nsal = sal + acr
+#print('O salário foi reajustado para {:.2f}'.format(nsal))
+
+#conversor C para F
+#temp = float(input('Informe a temperatura em C: '))
+#conv = (temp*1.8)+32
+#print('A temperatura em F é {}'.format(conv))
+
+#Aluguel carros
+#d = int(input('Quantos dias o carro foi alugado?: '))
+#dt = float(input('Quantos KM foram percorridos?: '))
+#v = (60 * d) + (dt * 0.15)
+#print('Preço total a ser pago: R${:.2f}'.format(v))
