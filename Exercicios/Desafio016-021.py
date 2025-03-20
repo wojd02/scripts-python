098f63c4dc81bc91d5cbff6b16983714be06c0ad
wojd02 <jonatanbayere@gmail.com>
2025-03-20 19:55:25 -0300

    Python exercícios 16-21

---
+++ Exercicios/Desafio016-021.py
@@ -0,0 +1,61 @@
+#from math import sqrt
+#n = int(input('Digite um número: '))
+#raiz = sqrt(n)
+#print('A raíz quadrada de {} é {}'.format(n, raiz))
+
+#import random
+#num = random.randint(1, 10)
+#print(num)
+
+#import emoji
+#print(emoji.emojize('Olá mundo :skull:', language='alias'))
+
+#Parte inteira número
+#from math import trunc
+#n = float(input('Digite um número: '))
+#inteiro = trunc(n)
+#print('A parte inteira fica {}'.format(inteiro))
+
+#Calculando a hipotenusa
+#from math import hypot
+#side1 = float(input('Digite o valor do cateto oposto: '))
+#side2 = float(input('Digite o valor do cateto adjascente: '))
+#hipotenusa = hypot(side1, side2)
+#print('A hipotenusa é {:.2f}'.format(hipotenusa))
+
+#Calculando cos, sen e tan através de um ângulo
+#import math
+#ang = float(input('Digite um ângulo qualquer: '))
+#c = math.cos(ang)
+#t = math.tan(ang)
+#s = math.sin(ang)
+#print('O cosseno é {:.2f}, seno é {:.2f} e a tangente é {:.1f}'.format(c, s, t))
+
+#Sorteio alunos apagar quadro
+#import random
+#nome1 = input('Digite o nome do primeiro aluno: ')
+#nome2 = input('Digite o nome do segundo aluno: ')
+#nome3 = input('Digite o nome do terceiro aluno: ')
+#nome4 = input('Digite o nome do quarto aluno: ')
+#lista_nomes = [nome1, nome2, nome3, nome4]
+#aluno = random.choice(lista_nomes)
+#print('O aluno sorteado para apagar o quadro foi {}'.format(aluno))
+
+#ordem aleatória sorteio alunos
+#import random
+#nome1 = input('Digite o nome do primeiro aluno: ')
+#nome2 = input('Digite o nome do segundo aluno: ')
+#nome3 = input('Digite o nome do terceiro aluno: ')
+#nome4 = input('Digite o nome do quarto aluno: ')
+#lista_nomes = [nome1, nome2, nome3, nome4]
+#aluno = sorted(lista_nomes)
+#print('A ordem de apresentação ficou {}'.format(aluno))
+
+#tocando música no python
+#import pygame
+#pygame.mixer.init()
+#pygame.init()
+#pygame.mixer.music.load('D:\programação\scripts-python\Exercicios\musica.mp3')
+#pygame.mixer.music.play()
+#pygame.event.wait()
+
