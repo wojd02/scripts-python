41253419283f469089aa0aa60429495994eb0362
wojd02 <jonatanbayere@gmail.com>
2025-03-21 14:07:00 -0300

    desafios python 22-25

---
+++ Exercicios/Desafio022.py
@@ -0,0 +1,26 @@
+#frase = 'Essa frase vai ser usada para diversos testes'
+#print(frase.lower().find('diversos'))
+
+#Analisador de nome
+#name = str (input('Digite seu nome completo: '))
+#newname = name.split()
+#newname2 =''.join(newname)
+#print('O nome digitado possui {} caracteres'.format(len(newname2)))
+#print(name.upper())
+#print(name.lower())
+#print('O primeiro nome tem {} letras'.format(name.find(' ')))
+#print(len(newname))
+
+#Ler unidade/dezenas/centenas
+#n = str(input('Digite um número: '))
+#print('Unidades {}'.format(n[-1]))
+#print('Dezenas {}'.format(n[-2]))
+#print('Centenas {}'.format(n[-3]))
+#print('Milhares {}'.format(n[-4]))
+
+#name = str (input('Digite o nome da cidade que você mora: '))
+#print(name[0:5] == 'santo')
+
+#name = input('Digite o seu nome: ')
+#print(name.lower().find('silva'))
+
