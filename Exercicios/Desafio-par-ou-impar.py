d4fa6453431090011580e93cc9c2a8ce75f17e6e
wojd02 <jonatanbayere@gmail.com>
2025-03-29 14:11:05 -0300

    desafios-068-069

---
+++ Exercicios/Desafio-par-ou-impar.py
@@ -0,0 +1,34 @@
+from random import randint
+from time import sleep
+cont = 0
+
+while True:
+    print('-=-' *20)
+    print('Vamos jogar par ou ímpar!')
+    print('-=-' *20)
+    decisao_pc = randint(0, 10)
+    decisao_player = int(input('Digite um número [0, 10]: '))
+    decisao_time = str(input('Você quer par ou ímpar [P/I]?: ')).upper()
+    soma = decisao_pc + decisao_player
+    print('1.. 2.. JOGAR!')
+    sleep(1)
+    print(f'Eu joguei {decisao_pc} e você jogou {decisao_player} e isso fica {soma} que é', end=' ')
+    print('PAR' if soma % 2 == 0  else 'ÍMPAR')
+    if soma % 2 == 0:
+        if decisao_time == 'P':
+            print('VOCÊ GANHOU!!')
+            cont += 1
+        elif decisao_time == 'I':
+            print('VOCÊ PERDEU!!')
+            print('-=-' *20)
+            break
+    if soma % 2 != 0:
+        if decisao_time == 'I':
+            print('VOCÊ GANHOU!!')
+            cont += 1
+        elif decisao_time == 'P':
+            print('VOCÊ PERDEU!!')
+            print('-=-' *20)
+            break
+print(f'Você ganhou {cont} vezes seguidas!')
+
---
+++ Exercicios/cadastro-pessoas.py
@@ -0,0 +1,31 @@
+from time import sleep
+cont_mais_18 = cont_homem = cont_mulher = 0
+decisao = 'S'
+
+while True:
+    print('-' * 30)
+    print('Cadastre uma pessoa:')
+    print('-' * 30)
+    if decisao == 'S':
+        sexo = str(input('Qual o sexo da pessoa? [F/M]: ')).upper()
+        while sexo not in 'FM':
+            print('Decisão inválida, tente novamente!')
+            sexo = str(input('Qual o sexo da pessoa? [F/M]: ')).upper()
+        idade = int(input('Qual a idade da pessoa?: '))
+        decisao = str(input('Gostaria de continuar? [S/N]')).upper()
+        while decisao not in 'SN':
+            print('Decisão inválida, tente novamente!')
+            decisao = str(input('Gostaria de continuar? [S/N]')).upper()
+        if idade >= 18:
+            cont_mais_18 += 1
+        if sexo == 'M':
+            cont_homem += 1
+        if sexo == 'F' and idade <= 20:
+            cont_mulher += 1
+    else:
+        print('ENCERRANDO O PROGRAMA...')
+        sleep(1)
+        break
+print(f'Tiveram {cont_mais_18} pessoas com mais de 18 anos')
+print(f'Tiveram {cont_homem} homens cadastrados')
+print(f'Foram cadastradas {cont_mulher} mulheres com menos de 20 anos')
