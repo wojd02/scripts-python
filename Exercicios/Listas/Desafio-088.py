b589df8afab53ef9639d5e7e04254be6c3a61739
wojd02 <jonatanbayere@gmail.com>
2025-04-08 11:54:07 -0300

    Final módulo listas

--- Exercicios/Listas/Desafio-088.py
+++ Exercicios/Listas/Desafio-088.py
@@ -1,3 +1,4 @@
+from time import sleep
 alunos = []
 juncao = []
 while True:
@@ -10,15 +11,23 @@ while True:
     alunos.append(juncao[:])
     juncao.clear()
     decisao = str(input('Gostaria de continuar [S/N]?: ')).upper()
+    while decisao not in 'SN':
+        print('Decisão inválida, tente novamente')
+        decisao = str(input('Gostaria de continuar [S/N]?: ')).upper()
     if decisao == 'N':
         break
-for s in alunos: #sistema media
-    for n in s:
-        media = (s[1] + s[2]) / 2
-    print(media)
-
-print('No. NOME MÉDIA')
-print('-' * 20)
-for ind, aluno in enumerate(0, len(alunos)):
-    print(f'{ind} {alunos[aluno][0]} {media}')
-
+print(f'{"No.":<4}{"Nome":<10}{"Média":>8}') #sistema layout
+print('-' * 26)
+for ind, aluno in enumerate (alunos):
+    print(f'{ind:<4}{aluno[0]:<10}{(aluno[1] + aluno[2]) / 2:>8.1f}')
+while True:
+    aluno_sep = int(input('Digite o número do aluno para ver as notas separadas (999 termina): '))
+    while aluno_sep not in range(0, len(alunos)): #Digitar número errado
+        print('Opção inválida, tente novamente!')
+        aluno_sep = int(input('Digite o número do aluno para ver as notas separadas (999 termina): '))
+    if aluno_sep != 999: #busca médias separadas
+        print(f'As notas do aluno(a) {alunos[aluno_sep][0]} são {alunos[aluno_sep][1]} e {alunos[aluno_sep][2]}')
+    if aluno_sep == 999: #Fim programa
+        print('Finalizando o programa...')
+        sleep(1)
+        break
