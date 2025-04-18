50a72db80efdfc6f2332a6bd4e7a2d95cfd64b62
wojd02 <jonatanbayere@gmail.com>
2025-04-18 15:59:42 -0300

    Começo-Modularização

---
+++ Exercicios/Modularização/Aula-01.py
@@ -0,0 +1,5 @@
+import defs
+numero = int(input('Digite um número para o fatorial: '))
+print(defs.fatorial(numero))
+print(defs.dobro(numero))
+print(defs.triplo(numero))
---
+++ Exercicios/Modularização/defs.py
@@ -0,0 +1,11 @@
+def fatorial(num):
+    fat = 1
+    for c in range(num, 1, -1):
+        fat *= c
+    return fat
+def dobro(num):
+   res = num * 2
+   return res
+def triplo(num):
+    resp = num * 3
+    return resp
