b72036938eaf07e647672bb952b259640b5997ee
wojd02 <jonatanbayere@gmail.com>
2025-05-09 11:34:34 -0300

    voltando python

--- Exercicios/teste-01.py
+++ Exercicios/teste-01.py
@@ -1,4 +1,8 @@
-nome = input("Qual é o seu nome?")
-idade = input("Qual sua idade?")
-peso - input("Qual é o seu peso")
-print(nome, idade, peso)
+#nome = input("Qual é o seu nome?")
+#idade = input("Qual sua idade?")
+#peso - input("Qual é o seu peso")
+#print(nome, idade, peso)
+#Retornando a parte float de um número
+num = float(input('Digite um número:'))
+resp = num - int(num)
+print(resp)
