259258115320faac43af3d8ddfbf5217f83241b8
wojd02 <jonatanbayere@gmail.com>
2025-05-23 18:20:28 -0300

    classes-python-5

--- Codecademy/inherit.py
+++ Codecademy/inherit.py
@@ -3,6 +3,8 @@ class Animais:
         self.localizaçao = localizaçao
     def andar(self):
         print(f'O animal esta andando por {self.localizaçao}')
+    def _dormir(self): #encapsulamento protegido, só pode ser acessado na classe e nas classes filhas
+        print('O animal esta dormindo')
 
 class Gato(Animais):
     def __init__(self, localizaçao):
@@ -16,9 +18,12 @@ class Cachorro(Animais):
     def latir(self):
         print('Roof')
         self.andar()
+        self._dormir()
 
 Miauzin = Gato('Arizona')
 Miauzin.miar()
 print()
 Mordiscado = Cachorro('Rio De Janeiro')
 Mordiscado.latir()
+print()
+Mordiscado._dormir() #Isso aqui não poderia acontecer, pois objetos não podem chamar métodos privados.
