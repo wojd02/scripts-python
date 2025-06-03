ec97e33862f81411b8f40feabbab57b74326aa49
wojd02 <jonatanbayere@gmail.com>
2025-06-03 18:39:19 -0300

    codecademy generators

---
+++ Codecademy/generators.py
@@ -0,0 +1,16 @@
+max_students = 50
+#método SEND, podemos enviar informações para dentro do gerador
+def get_id_students():
+    student_id = 1
+    while max_students >= student_id:
+        n = yield student_id
+        if n is not None:
+            student_id = n
+            continue
+        student_id += 1
+
+students = get_id_students()
+for id in students:
+    if id == 1:
+        id = students.send(25)
+    print(id)
--- Codecademy/iterators.py
+++ Codecademy/iterators.py
@@ -1,6 +1,29 @@
 import itertools
-num = int(input('Digite um número: '))
-for c in itertools.count(0,2):
+#num = int(input('Digite um número: '))
+#for c in itertools.count(0,2):
+#    print(c)
+#    if c >= num:
+#        break
+
+#lista = [1,7,4,3,6]
+#iter_list = lista.__iter__() #transforma o elemento na classe iterator
+#list_colors = ['red', 'blue', 'purple', 'cyan', 'green', 'black', 'white', 'yellow', 'orange', 'grey']
+#list_numbers = [1, 7, 3, 5, 2]
+#mix_colors_3 = itertools.combinations(list_colors, 3)
+#mix_numbers_2 = itertools.combinations(list_numbers, 2)
+#for mix in mix_numbers_2:
+#    print(mix) 
+
+#difference between YIELD and RETURN
+def teste():
+    yield 'look'
+    yield 'this'
+    yield 'string'
+
+def teste2():
+    return 'look'
+    return 'this'
+    return 'string'
+test = teste2()
+for c in test:
     print(c)
-    if c >= num:
-        break
