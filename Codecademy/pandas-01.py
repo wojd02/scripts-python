63de2ef68d608636d5ca702fe1f05202124eb407
wojd02 <jonatanbayere@gmail.com>
2025-06-25 20:15:18 -0300

    pandas-02

--- Codecademy/pandas-01.py
+++ Codecademy/pandas-01.py
@@ -27,4 +27,12 @@ cursos_tabela = pd.read_excel("C:/Users/Windows 11/Desktop/cursos_teste.xlsx")
 display(cursos_tabela)
 print()
 display(cursos_tabela.head()) #O .head(n de linhas) mostra as 5 primeiras(se n tiver outro numero no parenteses) linhas da tabela 
-
+print(cursos_tabela.shape) #mostra o numero de linhas e colunas
+print(cursos_tabela.describe()) #mostra dados da tabela
+tipos_curso = cursos_tabela[['Curso', 'Nomes']] #selecionando colunas para aparecer
+print(tipos_curso)
+print()
+print(cursos_tabela.loc[cursos_tabela['Curso'] == 'Artes visuais']) #selecionando elementos na tabela
+#cursos_tabela.loc[linhas, colunas] estrutura do LOC
+print()
+print(cursos_tabela.loc[cursos_tabela['Curso'] == 'Filosofia', ['Nomes', 'email', 'status aula']])
