fe1d61cc059cd6447f951cd4573cb073d2ad3a5a
wojd02 <jonatanbayere@gmail.com>
2025-08-20 19:07:41 -0300

    off-plataform-project-codecademy-02

--- Codecademy/off-plataform-project/index.py
+++ Codecademy/off-plataform-project/index.py
@@ -1,6 +1,4 @@
 import csv
-import pandas as pd
-import matplotlib.pyplot as plt
 #goals
  # average total charge and by region
  # total of smoker and non-smoker
@@ -22,14 +20,17 @@ def list_joint(list_1, list_2, list_3, list_4, list_5, list_6, list_7):
 
 def charge_region(region_list, charge_list):
     charges_per_region = {}
+    charges_per_region_2 = {}
     for key, region in enumerate(region_list):
         if region not in charges_per_region:
             charges_per_region[region] = [charge_list[key]]
         elif region in charges_per_region:
             charges_per_region[region].append(charge_list[key])
     for key in charges_per_region:
+        charges_per_region_2[key] = sum(charges_per_region[key])
+    for key in charges_per_region:
         charges_per_region[key] = sum(charges_per_region[key]) / len(charges_per_region[key])
-    return charges_per_region
+    return charges_per_region, charges_per_region_2
 
 
 #converting csv to dictionary
@@ -54,6 +55,7 @@ total = sum(charges) / len(charges)
 print(f'AVG total: {total:.2f} USD')
 
 #average per region
-charge_per_region = charge_region(region, charges)
+charge_per_region, charge_per_region_2 = charge_region(region, charges)
+print(charge_per_region_2)
 for row in charge_per_region.items():
     print(f'The average charge in the {row[0]} area is {row[-1]:,.2f} USD')
